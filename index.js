ca84a842c0988a5a51475aa78b2df50edce0a70a
bmiit145 <21bmiit145@gmail.com>
2023-08-19 13:01:12 +0530

    personal chat application

---
+++ index.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const http = require('http');
+const socketIO = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIO(server);
+
+const PORT = process.env.PORT || 3000;
+
+app.use(express.static(__dirname + '/public'));
+
+const messages = [];
+
+io.on('connection', (socket) => {
+  socket.on('message', (message) => {
+    messages.push({ content: message, timestamp: Date.now() });
+
+    // Broadcast the received message to all connected clients
+    io.emit('message', message);
+
+    // Schedule the message to be deleted after 2 minutes
+    setTimeout(() => {
+      const index = messages.findIndex((msg) => msg.content === message);
+      if (index !== -1) {
+        messages.splice(index, 1);
+        io.emit('deleteMessage', message);
+      }
+    }, 4 * 1000); // 2 minutes in milliseconds
+  });
+});
+
+server.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
